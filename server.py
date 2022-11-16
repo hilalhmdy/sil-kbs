15a9be86462e4d9be67f9698908b262cf9593736
hilalhmdy <13519024@std.stei.itb.ac.id>
2022-11-16 10:48:25 +0700

    add

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 from cgitb import html
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
 def hello_world():
     pred = model.predict([2,0,0,2,1,5,41,0,1,0,1,2,2,1,1,0,False,False,False,False,False])
     return "<p>Result: "+ str(pred) +"</p>"
 
-# @app.route('/')
-# def homepage():
-#     return render_template('homepage.html')
+@app.route('/')
+def homepage():
+    print(request.method)
+    if (request.method == 'POST'):
+        return "<p> Hello world </p>"
 
+    if (request.method == 'GET'):
+        return render_template('homepage.html')
+    
 
 if __name__ == '__main__':
     model.init()
